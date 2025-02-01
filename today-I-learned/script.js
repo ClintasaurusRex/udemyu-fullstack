c31c4a84fdeb6e59cdf2d770bc6b129ef3aba628
Clint <clinthiles1992@gmail.com>
2025-02-01 15:22:47 -0800

    Add form toggle functionality with JavaScript

---
+++ today-I-learned/script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('.btn-open');
+const form = document.querySelector('.fact-form');
+
+btn.addEventListener('click', function () {
+  if (form.classList.contains('hidden')) {
+    form.classList.remove('hidden');
+    btn.textContent = 'Close';
+  } else {
+    form.classList.add('hidden');
+    btn.textContent = 'Share a fact';
+  }
+});
+
+function calcAge(year) {
+  const age = 2024 - year;
+
+  const msg = age > 19 ? 'You can vote' : 'You are not old enough to vote';
+  return msg;
+}
+
+console.log(calcAge(2023));
