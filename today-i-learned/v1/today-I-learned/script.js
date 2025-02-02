1dc25a877b361ef7f1377c60bdf47147fcc8aa02
Clint <clinthiles1992@gmail.com>
2025-02-01 19:50:50 -0800

    Add environment variable support for Supabase API credentials

--- today-i-learned/v1/today-I-learned/script.js
+++ today-i-learned/v1/today-I-learned/script.js
@@ -59,10 +59,8 @@ async function loadFacts() {
     'https://hhedmupgupwvjrohkvhr.supabase.co/rest/v1/facts',
     {
       headers: {
-        apikey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoZWRtdXBndXB3dmpyb2hrdmhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0NDc3MjksImV4cCI6MjA1NDAyMzcyOX0.0WPZbEbxZw4h_mAselhuKhGQ_PP1P6lZ7i4XCT2hM_E',
-        authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoZWRtdXBndXB3dmpyb2hrdmhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg0NDc3MjksImV4cCI6MjA1NDAyMzcyOX0.0WPZbEbxZw4h_mAselhuKhGQ_PP1P6lZ7i4XCT2hM_E',
+        apikey: process.env.SUPABASE_API_KEY,
+        authorization: process.env.SUPABASE_AUTH_TOKEN,
       },
     }
   );
